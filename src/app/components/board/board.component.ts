0a8f4186fb27bff770d728bb975a2c4b449697c0
Kanchukatla Divya <Kanchukatla.Divya@bakkt.com>
2023-11-26 06:27:52 -0800

    pushed recent code

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -18,7 +18,6 @@ export class BoardComponent implements OnInit {
   todo: any[] = []
   progress: any[] = [];
   done: any[] = [];
- 
   projectList: any[] = [];
   ticketsArray: any[] = [];
   status: string[] = ['To Do', 'In Progress', 'Done'];
@@ -40,34 +39,25 @@ export class BoardComponent implements OnInit {
     console.log(this.searchInput)
     this.filterBySearchInput(this.searchInput)
   }
-
+ 
   filterBySearchInput(value: any) {
     if (value) {
       const filteredArray = this.ticketsArray.filter(
         (item) => item.projectName.toLowerCase().includes(value.toLowerCase())
       );
-  
-     
       this.done = filteredArray.filter((m) => m.status === 'Done');
       this.todo = filteredArray.filter((m) => m.status === 'To Do');
       this.progress = filteredArray.filter((m) => m.status === 'In Progress');
-  
-      
       this.ticketsArray = filteredArray;
     } else {
-     
       this.ticketService.projectTicketsArray$.subscribe((tickets) => {
         this.ticketsArray = tickets;})
       this.done = this.ticketsArray.filter((m) => m.status === 'Done');
       this.todo = this.ticketsArray.filter((m) => m.status === 'To Do');
-      this.progress = this.ticketsArray.filter((m) => m.status === 'In Progress');
-      
-     
-    }
-  
+      this.progress = this.ticketsArray.filter((m) => m.status === 'In Progress');           
+    } 
   }
   
-
   getTasksByStatus(status: string): any[] {
     switch (status) {
       case 'To Do':
